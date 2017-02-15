529e61ebecb16b8763473b25e2ed1702fd6ec681
MounikaPrathipati <mounika16f@gmail.com>
2017-02-15 17:24:29 -0600

    updated

---
+++ Tutorial4/Documentation/TranslateApp/app/src/main/java/tutorial/cs5551/com/translateapp/RegisterActivity.java
@@ -0,0 +1,34 @@
+package tutorial.cs5551.com.translateapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void registerPage1(View v)
+    {
+           //This code redirects the from login page to the home page.
+            Intent signup1 = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(signup1);
+
+    }
+
+}
